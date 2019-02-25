70292cae6ab4f24a15db161c4b4f44b21c8ef34a
nicholasbohdan <43612367+nicholasbohdan@users.noreply.github.com>
2019-02-25 23:43:03 +0700

    Add files via upload

---
+++ latihan3.js
@@ -0,0 +1,330 @@
+const results = [
+        {
+            "name": "Alderaan", 
+            "rotation_period": "24", 
+            "orbital_period": "364", 
+            "diameter": "12500", 
+            "climate": "temperate", 
+            "gravity": "1 standard", 
+            "terrain": "grasslands, mountains", 
+            "surface_water": "40", 
+            "population": "2000000000", 
+            "residents": [
+                "https://swapi.co/api/people/5/", 
+                "https://swapi.co/api/people/68/", 
+                "https://swapi.co/api/people/81/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/6/", 
+                "https://swapi.co/api/films/1/"
+            ], 
+            "created": "2014-12-10T11:35:48.479000Z", 
+            "edited": "2014-12-20T20:58:18.420000Z", 
+            "url": "https://swapi.co/api/planets/2/"
+        }, 
+        {
+            "name": "Yavin IV", 
+            "rotation_period": "24", 
+            "orbital_period": "4818", 
+            "diameter": "10200", 
+            "climate": "temperate, tropical", 
+            "gravity": "1 standard", 
+            "terrain": "jungle, rainforests", 
+            "surface_water": "8", 
+            "population": "1000", 
+            "residents": [], 
+            "films": [
+                "https://swapi.co/api/films/1/"
+            ], 
+            "created": "2014-12-10T11:37:19.144000Z", 
+            "edited": "2014-12-20T20:58:18.421000Z", 
+            "url": "https://swapi.co/api/planets/3/"
+        }, 
+        {
+            "name": "Hoth", 
+            "rotation_period": "23", 
+            "orbital_period": "549", 
+            "diameter": "7200", 
+            "climate": "frozen", 
+            "gravity": "1.1 standard", 
+            "terrain": "tundra, ice caves, mountain ranges", 
+            "surface_water": "100", 
+            "population": "unknown", 
+            "residents": [], 
+            "films": [
+                "https://swapi.co/api/films/2/"
+            ], 
+            "created": "2014-12-10T11:39:13.934000Z", 
+            "edited": "2014-12-20T20:58:18.423000Z", 
+            "url": "https://swapi.co/api/planets/4/"
+        }, 
+        {
+            "name": "Dagobah", 
+            "rotation_period": "23", 
+            "orbital_period": "341", 
+            "diameter": "8900", 
+            "climate": "murky", 
+            "gravity": "N/A", 
+            "terrain": "swamp, jungles", 
+            "surface_water": "8", 
+            "population": "unknown", 
+            "residents": [], 
+            "films": [
+                "https://swapi.co/api/films/2/", 
+                "https://swapi.co/api/films/6/", 
+                "https://swapi.co/api/films/3/"
+            ], 
+            "created": "2014-12-10T11:42:22.590000Z", 
+            "edited": "2014-12-20T20:58:18.425000Z", 
+            "url": "https://swapi.co/api/planets/5/"
+        }, 
+        {
+            "name": "Bespin", 
+            "rotation_period": "12", 
+            "orbital_period": "5110", 
+            "diameter": "118000", 
+            "climate": "temperate", 
+            "gravity": "1.5 (surface), 1 standard (Cloud City)", 
+            "terrain": "gas giant", 
+            "surface_water": "0", 
+            "population": "6000000", 
+            "residents": [
+                "https://swapi.co/api/people/26/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/2/"
+            ], 
+            "created": "2014-12-10T11:43:55.240000Z", 
+            "edited": "2014-12-20T20:58:18.427000Z", 
+            "url": "https://swapi.co/api/planets/6/"
+        }, 
+        {
+            "name": "Endor", 
+            "rotation_period": "18", 
+            "orbital_period": "402", 
+            "diameter": "4900", 
+            "climate": "temperate", 
+            "gravity": "0.85 standard", 
+            "terrain": "forests, mountains, lakes", 
+            "surface_water": "8", 
+            "population": "30000000", 
+            "residents": [
+                "https://swapi.co/api/people/30/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/3/"
+            ], 
+            "created": "2014-12-10T11:50:29.349000Z", 
+            "edited": "2014-12-20T20:58:18.429000Z", 
+            "url": "https://swapi.co/api/planets/7/"
+        }, 
+        {
+            "name": "Naboo", 
+            "rotation_period": "26", 
+            "orbital_period": "312", 
+            "diameter": "12120", 
+            "climate": "temperate", 
+            "gravity": "1 standard", 
+            "terrain": "grassy hills, swamps, forests, mountains", 
+            "surface_water": "12", 
+            "population": "4500000000", 
+            "residents": [
+                "https://swapi.co/api/people/3/", 
+                "https://swapi.co/api/people/21/", 
+                "https://swapi.co/api/people/36/", 
+                "https://swapi.co/api/people/37/", 
+                "https://swapi.co/api/people/38/", 
+                "https://swapi.co/api/people/39/", 
+                "https://swapi.co/api/people/42/", 
+                "https://swapi.co/api/people/60/", 
+                "https://swapi.co/api/people/61/", 
+                "https://swapi.co/api/people/66/", 
+                "https://swapi.co/api/people/35/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/5/", 
+                "https://swapi.co/api/films/4/", 
+                "https://swapi.co/api/films/6/", 
+                "https://swapi.co/api/films/3/"
+            ], 
+            "created": "2014-12-10T11:52:31.066000Z", 
+            "edited": "2014-12-20T20:58:18.430000Z", 
+            "url": "https://swapi.co/api/planets/8/"
+        }, 
+        {
+            "name": "Coruscant", 
+            "rotation_period": "24", 
+            "orbital_period": "368", 
+            "diameter": "12240", 
+            "climate": "temperate", 
+            "gravity": "1 standard", 
+            "terrain": "cityscape, mountains", 
+            "surface_water": "unknown", 
+            "population": "1000000000000", 
+            "residents": [
+                "https://swapi.co/api/people/34/", 
+                "https://swapi.co/api/people/55/", 
+                "https://swapi.co/api/people/74/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/5/", 
+                "https://swapi.co/api/films/4/", 
+                "https://swapi.co/api/films/6/", 
+                "https://swapi.co/api/films/3/"
+            ], 
+            "created": "2014-12-10T11:54:13.921000Z", 
+            "edited": "2014-12-20T20:58:18.432000Z", 
+            "url": "https://swapi.co/api/planets/9/"
+        }, 
+        {
+            "name": "Kamino", 
+            "rotation_period": "27", 
+            "orbital_period": "463", 
+            "diameter": "19720", 
+            "climate": "temperate", 
+            "gravity": "1 standard", 
+            "terrain": "ocean", 
+            "surface_water": "100", 
+            "population": "1000000000", 
+            "residents": [
+                "https://swapi.co/api/people/22/", 
+                "https://swapi.co/api/people/72/", 
+                "https://swapi.co/api/people/73/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/5/"
+            ], 
+            "created": "2014-12-10T12:45:06.577000Z", 
+            "edited": "2014-12-20T20:58:18.434000Z", 
+            "url": "https://swapi.co/api/planets/10/"
+        }, 
+        {
+            "name": "Geonosis", 
+            "rotation_period": "30", 
+            "orbital_period": "256", 
+            "diameter": "11370", 
+            "climate": "temperate, arid", 
+            "gravity": "0.9 standard", 
+            "terrain": "rock, desert, mountain, barren", 
+            "surface_water": "5", 
+            "population": "100000000000", 
+            "residents": [
+                "https://swapi.co/api/people/63/"
+            ], 
+            "films": [
+                "https://swapi.co/api/films/5/"
+            ], 
+            "created": "2014-12-10T12:47:22.350000Z", 
+            "edited": "2014-12-20T20:58:18.437000Z", 
+            "url": "https://swapi.co/api/planets/11/"
+        }
+    ]
+let table = document.getElementById("body");
+    mapping(results);
+    function search(){
+        let keyword =  document.getElementById('search').value.toLowerCase();
+        let option =  document.getElementById('option').value.toLowerCase();
+        let sort =  document.getElementById('sort').value.toLowerCase();
+        let newarray = [];
+        switch (option){
+            case 'name' :
+                newarray = results.filter(object => object.name.toLowerCase().indexOf(keyword) > -1);
+                newarray = sortNameString(newarray,sort);
+                break;
+            case 'rotation':
+                newarray = results.filter(object => object.rotation_period.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'orbital':
+                newarray = results.filter(object => object.orbital_period.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'diameter':
+                newarray = results.filter(object => object.diameter.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'climate':
+                newarray = results.filter(object => object.climate.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'gravity':
+                newarray = results.filter(object => object.gravity.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'terrain':
+                newarray = results.filter(object => object.terrain.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'surface':
+                newarray = results.filter(object => object.surface_water.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'population':
+                newarray = results.filter(object => object.population.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'created':
+                newarray = results.filter(object => object.created.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'edited':
+                newarray = results.filter(object => object.edited.toLowerCase().indexOf(keyword) > -1);
+                break;
+            case 'url':
+                newarray = results.filter(object => object.url.toLowerCase().indexOf(keyword) > -1);
+                break;
+        }
+        mapping(newarray);
+    }
+    function sortRotation(arr,sort){
+        if(sort === 'asc'){
+            arr.sort(function(a,b){
+                return a.rotation_period-b.rotation_period
+            })
+        }
+    }
+    function sortNameString(arr,sort){
+        if(sort === 'asc'){
+            arr.sort(function(a, b){
+                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
+                if (nameA > nameB) //sort string ascending
+                    return -1;
+                if (nameA < nameB)
+                    return 1;
+                return 0 //default return value (no sorting)
+            });
+        }
+        else{
+            arr.sort(function(a, b){
+                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
+                if (nameA < nameB) //sort string ascending
+                    return -1;
+                if (nameA > nameB)
+                    return 1;
+                return 0 //default return value (no sorting)
+            });
+        }
+        return arr;
+    }
+    function mapping (arr){
+        document.getElementById("body").innerHTML = "";
+        arr.map(function(row,key){
+            var x = document.getElementById("body").rows.length;
+            let tablerow = table.insertRow(x);
+            let cell1 = tablerow.insertCell(0);
+            let cell2 = tablerow.insertCell(1);
+            let cell3 = tablerow.insertCell(2);
+            let cell4 = tablerow.insertCell(3);
+            let cell5 = tablerow.insertCell(4);
+            let cell6 = tablerow.insertCell(5);
+            let cell7 = tablerow.insertCell(6);
+            let cell8 = tablerow.insertCell(7);
+            let cell9 = tablerow.insertCell(8);
+            let cell10 = tablerow.insertCell(9);
+            let cell11 = tablerow.insertCell(10);
+            let cell12 = tablerow.insertCell(11);
+            cell1.innerHTML = row.name;
+            cell2.innerHTML = row.rotation_period;
+            cell3.innerHTML = row.orbital_period;
+            cell4.innerHTML = row.diameter;
+            cell5.innerHTML = row.climate;
+            cell6.innerHTML = row.gravity;
+            cell7.innerHTML = row.terrain;
+            cell8.innerHTML = row.surface_water;
+            cell9.innerHTML = row.population;
+            cell10.innerHTML = row.created;
+            cell11.innerHTML = row.edited;
+            cell12.innerHTML = row.url;
+        });
+    }
